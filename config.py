d3cf298c6c669ba0c6e67e5e58b65707324b2237
IsmailAtilTURK <admin@by-turco.com>
2025-04-09 19:49:07 +0300

    fix config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ import secrets
 class Config:
     SECRET_KEY = 'c61c30dd38801ec0dd1de66248171432689dd1f8c8abc08e7924a9df2c543843'
     # MySQL veritabanı bağlantı bilgileri
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Asli281019*Cagdas@localhost:3306/backlink'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:SrvzfKBHfZkNkPMgZBPKLlVkTKBLEMLh@mysql.railway.internal:3306/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MIN_PAGE_RANK = 3
